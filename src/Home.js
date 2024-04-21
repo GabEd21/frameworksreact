cda65201bfdfe9bbb91204335ee06c861c630f76
Rodwell <matchon.rodwell@gmail.com>
2024-04-21 23:40:28 +0800

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Home(){
+    return(
+        <div>
+            <h1>Welcome to my world.</h1>
+        </div>
+    )
+}
+
+export default Home;
